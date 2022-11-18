37ee37bb1685aa0155bd61d33b579da93d073e25
owen-bryan <odog654@gmail.com>
2022-11-18 17:25:21 -0500

    work done on program

--- src/Components/addCard.js
+++ src/Components/addCard.js
@@ -1,7 +1,22 @@
-let AddCard = () => {
+import { useState } from "react"
+
+const AddCard = () => {
+    const [getCard, setCard] = useState("")
+    
+    const inputHandler = (event) =>
+    {
+        setCard (event.target.value)
+    }
+
+    const submitHandler = (event) =>
+    {
+        console.log('getCard()', getCard)
+    }
+
     return (
         <div>
-            <h1>hello World</h1>
+            <input type="text" onChange={inputHandler}/>
+            <button onClick={submitHandler} type="submit">Submit</button>
         </div>
     )
 }
--- src/Components/displayCards.js
+++ src/Components/displayCards.js
@@ -1,5 +1,5 @@
 
-let DisplayCards = () =>
+const DisplayCards = () =>
 {
     return (
         <div>
