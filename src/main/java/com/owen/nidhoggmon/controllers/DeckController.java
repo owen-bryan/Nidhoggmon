e85a01b3be2fe922a3c9cdb2b2d7dc1149ebb885
Owen B <owen.b@example.com>
2024-05-12 21:45:29 -0400

    Fixed inserting new decks into database.

--- src/main/java/com/owen/nidhoggmon/controllers/DeckController.java
+++ src/main/java/com/owen/nidhoggmon/controllers/DeckController.java
@@ -1,6 +1,5 @@
 package com.owen.nidhoggmon.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -10,11 +9,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.owen.nidhoggmon.models.Card;
 import com.owen.nidhoggmon.models.Deck;
 import com.owen.nidhoggmon.services.DeckListService;
 
@@ -22,7 +20,6 @@ import com.owen.nidhoggmon.services.DeckListService;
 public final class DeckController {
 
     private final static Logger logger = LoggerFactory.getLogger(DeckController.class);
-
 
     @Autowired
     private DeckListService service;
@@ -42,31 +39,25 @@ public final class DeckController {
 
     @GetMapping("/deck/{id}")
     @ResponseBody
-    public String getDeck(@PathVariable(value = "id") long id) {
+    public String getDeck(@PathVariable(value = "id") String id) {
         logger.info(String.format("Getting ID = %x", id));
         return "ID: " + id;
     }
 
     @DeleteMapping("/deck/{id}")
-    public void deleteDeck(@PathVariable(value = "id") long id) {
+    public void deleteDeck(@PathVariable(value = "id") String id) {
         logger.info(String.format("Deleting ID = %x", id));
     }
 
     @DeleteMapping("/deck/{id}/{cardId}")
-    public void deleteCard(@PathVariable(value = "id") long id, @PathVariable(value = "cardId") String cardId) {
+    public void deleteCard(@PathVariable(value = "id") String id, @PathVariable(value = "cardId") String cardId) {
         logger.info(String.format("Removing Card ID = %s from Deck ID = %x", cardId, id));
     }
 
     @PostMapping({ "/deck", "/deck/" })
     @ResponseBody
-    public boolean newDeck(@RequestParam("name") String name, @RequestParam("main_deck") List<Card> main_deck, @RequestParam("egg_deck") List<Card> egg_deck, @RequestParam("count") int count) {
-        
-        logger.debug(name);
-        logger.debug(main_deck.toString());
-        logger.debug(egg_deck.toString());
-        logger.debug("" + count);
-        Deck deck = new Deck (name, main_deck, egg_deck, count);
-        
+    public boolean newDeck(@RequestBody Deck deck) {
+
         return service.newDeck(deck);
     }
 }
